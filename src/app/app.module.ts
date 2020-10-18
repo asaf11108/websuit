0fb842b14af061b7b31181bc978104fa88639554
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2020-10-18 16:47:15 +0300

    added i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { I18nModule } from './i18n/i18n.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    I18nModule
   ],
   providers: [],
   bootstrap: [AppComponent]
