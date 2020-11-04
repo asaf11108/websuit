34a0758a85185e79a6a259005b643d20c455e07b
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2020-11-04 10:08:57 +0200

    added info and bootstrap

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -4,12 +4,14 @@ import { BrowserTransferStateModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { I18nBrowserModule } from './i18n/i18n.browser.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     I18nBrowserModule,
     AppModule,
-    BrowserTransferStateModule
+    BrowserTransferStateModule,
+    NgbModule
   ],
   bootstrap: [AppComponent],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { SelectLanguageComponent } from './select-language/select-language.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularTiltModule } from 'angular-tilt';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IntroComponent } from './intro/intro.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SelectLanguageComponent
+    SelectLanguageComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     AngularTiltModule,
     TranslateModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
